87804c57265cdda0830cae54a33050fbb234c1b5
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-12-16 15:59:48 -0300

    :construction: feat: create utils function to check period and refactor action

--- app/actions.ts
+++ app/actions.ts
@@ -3,6 +3,7 @@
 import { prisma } from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
 import z from 'zod';
+import { calculatePeriod } from '@/app/utils/appointment-utils';
 
 const appointmentSchema = z.object({
   tutorName: z.string(),
@@ -27,9 +28,7 @@ export async function createAppointment(data: AppointmentData) {
     const { scheduleAt } = parsedData;
     const hour = scheduleAt.getHours();
 
-    const isMorning = hour >= 9 && hour < 12;
-    const isAfternoon = hour >= 13 && hour < 18;
-    const isEvening = hour >= 19 && hour < 21;
+    const { isMorning, isAfternoon, isEvening } = calculatePeriod(hour);
 
     if (!isMorning && !isAfternoon && !isEvening) {
       return {
@@ -82,9 +81,7 @@ export async function updateAppointment(id: string, data: AppointmentData) {
     const { scheduleAt } = parsedData;
     const hour = scheduleAt.getHours();
 
-    const isMorning = hour >= 9 && hour < 12;
-    const isAfternoon = hour >= 13 && hour < 18;
-    const isEvening = hour >= 19 && hour < 21;
+    const { isMorning, isAfternoon, isEvening } = calculatePeriod(hour);
 
     if (!isMorning && !isAfternoon && !isEvening) {
       return {
