29ddd89117239b3d6bfe9639eab81f594d73326f
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-12-16 15:32:56 -0300

    :construction: feat: format with prettier

--- app/components/date-picker/date-picker.tsx
+++ app/components/date-picker/date-picker.tsx
@@ -1,9 +1,24 @@
 'use client';
 
-import { Calendar, ChevronDown, ChevronLeft, ChevronRight } from 'lucide-react';
+import {
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  Calendar as CalendarIcon,
+} from 'lucide-react';
+import { Calendar } from '@/app/components/ui/calendar';
 import { Button } from '@/app/components/ui/button';
-import { Popover, PopoverTrigger } from '@/app/components/ui/popover';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/app/components/ui/popover';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useState, useCallback } from 'react';
+import { addDays, format, isValid } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { useEffect } from 'react';
+import { NavigationButton } from './navigation-button';
 
 export const DatePicker = () => {
   const router = useRouter();
@@ -11,30 +26,88 @@ export const DatePicker = () => {
   const searchParams = useSearchParams();
   const dateParam = searchParams.get('date');
 
+  const getInitialDate = useCallback(() => {
+    if (!dateParam) return;
+
+    const [year, month, day] = dateParam.split('-').map(Number);
+    const parsedDate = new Date(year, month - 1, day);
+
+    if (!isValid(parsedDate)) return new Date();
+
+    return parsedDate;
+  }, [dateParam]);
+
+  const [date, setDate] = useState<Date | undefined>(getInitialDate);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const updateURLWithDate = (selectedDate: Date | undefined) => {
+    if (!selectedDate) return;
+
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set('date', format(selectedDate, 'yyyy-MM-dd'));
+
+    router.push(`${pathname}?${newParams.toString()}`);
+  };
+
+  const handleNavigateDay = (days: number) => {
+    const newDate = addDays(date || new Date(), days);
+    updateURLWithDate(newDate);
+  };
+
+  const handleDateSelect = (selectedDate: Date | undefined) => {
+    updateURLWithDate(selectedDate);
+    setIsPopoverOpen(false);
+  };
+
+  useEffect(() => {
+    const newDate = getInitialDate();
+    setDate(newDate);
+  }, [getInitialDate]);
+
   return (
     <div className="flex items-center gap-2">
-      <Button variant="outline">
+      <NavigationButton
+        tooltipText="Dia anterior"
+        onClick={() => handleNavigateDay(-1)}
+      >
         <ChevronLeft className="h-4 w-4" />
-      </Button>
+      </NavigationButton>
 
-      <Popover>
+      <Popover onOpenChange={setIsPopoverOpen} open={isPopoverOpen}>
         <PopoverTrigger asChild>
           <Button
             variant="outline"
             className="w-min[180px] justify-between text-left font-normal bg-transparent border-border-primary text-content-primary hover:bg-background-tertiary hover:border-border-secondary hover:text-content-primary focus-visible:ring-offset-0 focus-visible:ring-1 focus-visible:ring-border-brand focus:border-border-brand focus-visible:border-border-brand"
           >
             <div className="flex items-center gap-2">
-              <Calendar className="h-4 w-4 text-content-brand" />
-              <span>Selecione uma data</span>
+              <CalendarIcon className="h-4 w-4 text-content-brand" />
+              {date ? (
+                format(date, 'dd/MM/yyyy')
+              ) : (
+                <span>Selecione uma data</span>
+              )}
             </div>
             <ChevronDown className="h-4 w-4 opacity-50" />
           </Button>
         </PopoverTrigger>
+
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={handleDateSelect}
+            autoFocus
+            locale={ptBR}
+          />
+        </PopoverContent>
       </Popover>
 
-      <Button variant="outline">
+      <NavigationButton
+        tooltipText="Próximo dia"
+        onClick={() => handleNavigateDay(1)}
+      >
         <ChevronRight className="h-4 w-4" />
-      </Button>
+      </NavigationButton>
     </div>
   );
 };
