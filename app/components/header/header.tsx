8acccd9b15cb0c5c8d58d2400fc183cc6b1f48fc
Pedro Henrique <pedruhbl@yahoo.com.br>
2025-12-17 14:37:36 -0300

    :construction: feat: add header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -3,8 +3,8 @@ import { Logo } from '../logo';
 export const Header = () => {
   return (
     <header
-      className="fixed top-0 z-50 w-full bg-background-primary/95 backdrop-blur
-      supports-backdrop-filter:bg-background-primary/60"
+      className="fixed top-0 z-50 w-full bg-background/95 backdrop-blur
+      supports-backdrop-filter:bg-background/60"
     >
       <Logo />
     </header>
